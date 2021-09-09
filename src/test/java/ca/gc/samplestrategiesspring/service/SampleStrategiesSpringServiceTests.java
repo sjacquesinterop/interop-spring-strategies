c3b3c8c5ce7b8baa0f445c7091d3a36b9bfad265
S.Jacques <sjacques230693@gmail.com>
2021-09-09 14:53:39 -0400

    added code coverage

--- src/test/java/ca/gc/samplestrategiesspring/service/SampleStrategiesSpringServiceTests.java
+++ src/test/java/ca/gc/samplestrategiesspring/service/SampleStrategiesSpringServiceTests.java
@@ -2,13 +2,35 @@ package ca.gc.samplestrategiesspring.service;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class SampleStrategiesSpringServiceTests {
 
+    @Autowired
+    SampleStrategiesSpringService sampleStrategiesSpringService;
+
     @Test
     public void getTest() {
-        Assertions.assertEquals(1, 1);
+        Assertions.assertEquals("Hello from Spring GET",
+                sampleStrategiesSpringService.sampleGet());
+    }
+
+    @Test
+    public void postTest() {
+        Assertions.assertEquals("Hello from Spring POST",
+                sampleStrategiesSpringService.samplePost());
+    }
+
+    @Test
+    public void putTest() {
+        Assertions.assertEquals("Hello from Spring PUT",
+                sampleStrategiesSpringService.samplePut());
+    }
+
+    @Test
+    public void deleteTest() {
+        sampleStrategiesSpringService.sampleDelete();
     }
 }
