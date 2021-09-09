bb078623297ead577fd10c9163005e66c549d191
S.Jacques <sjacques230693@gmail.com>
2021-09-09 14:21:27 -0400

    updated unit test package

---
+++ src/test/java/ca/gc/samplestrategiesspring/service/SampleStrategiesSpringServiceTests.java
@@ -0,0 +1,14 @@
+package ca.gc.samplestrategiesspring.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SampleStrategiesSpringServiceTests {
+
+    @Test
+    public void getTest() {
+        Assertions.assertEquals(1, 2);
+    }
+}
