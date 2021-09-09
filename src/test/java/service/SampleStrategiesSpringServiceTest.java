49bfbefc3b47f1caa828ee7f6f3fa7515404c9d1
S.Jacques <sjacques230693@gmail.com>
2021-09-09 12:10:16 -0400

    added unit test

---
+++ src/test/java/service/SampleStrategiesSpringServiceTest.java
@@ -0,0 +1,12 @@
+package service;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SampleStrategiesSpringServiceTest {
+
+    @Test
+    public void testGet() {
+        Assert.assertEquals(1, 2);
+    }
+}
